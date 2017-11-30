9862d7e694c4f5071accf62df86f1f57f9758f9e
Fazreil <fazreil@gmail.com>
2017-11-30 20:08:20 +0800

    papers

---
+++ lecture3/Continuous_Deployment_for_Complex_Software_Intensive_Industrial_Systems.js
@@ -0,0 +1,45 @@
+#direction: right
+
+[Title|
+Continuous Deployment for Complex Software Intensive Industrial Systems
+]
+
+[Year|
+2017
+]
+
+[Authors|
+	[Haider, Zulqarnain]
+]
+
+[Abstract|
+Processes to develop and deliver software have been evolved over the years. One of the primary motivations 
+of this evolution, is gaining the beneﬁts of shorter time-to-market. Continuous deployment is a recent 
+trend to deploy software to the customers automatically and in continuous fashion. Organizations adopting 
+this trend could reach the customers faster through quick deliveries and improve the quality and productivity 
+of the delivered product by an early feedback, and hence achieve increased customer satisfaction. Complex 
+software intensive industrial systems are large-scale, distributed over heterogeneous platforms and interact 
+with several sensors and actuators. Enabling continuous deployment for these industrial systems needs 
+a stable deployment process able to cope with domain speciﬁc requirements and challenges. Notably, the 
+required quality attributes of the deployed software product as well as the challenges introduced by 
+the customer-speciﬁc nature of the domain. In this thesis, we formalize continuous deployment for industrial 
+systems by identifying the main factors of an appropriate deployment process. In particular, we investigate 
+high-level requirements, required quality attributes of the software product, and challenges in the deployment.
+ Based on this, we propose a continuous deployment pipeline and a set of activities incorporated in the 
+ stages of the pipeline, in particular deployment and post-deployment stages. Moreover, we suggest automation 
+ support for the activities to both shorten the delivery time and to preserve repeatability and reliability 
+ of the deployment process. The aim of such a process is to maintain the quality attributes of the deployed 
+ software. We perform a case study to validate the proposed model by implementing a prototype in an industrial system
+]
+
+[Keywords]->[Continuous Deployment]
+[Keywords]->[Automated Deployment]
+[Keywords]->[Complex Software ]
+[Keywords]->[Intensive Industrial Systems]
+
+
+
+[Title]->[Year]
+[Title]->[Abstract]
+[Abstract]->[Keywords]
+[Title]-[Authors]
---
+++ lecture3/Enabling_DevOps_Collaboration_and_Continuous_Delivery_Using_Diverse_Application_Environments.js
@@ -0,0 +1,38 @@
+[Title|
+Enabling DevOps Collaboration and Continuous Delivery Using Diverse Application Environments
+]
+
+[Year|
+2017
+]
+
+[Authors|
+	[Johannes Wettinger]
+    [Vasilios Andrikopoulos]
+    [Frank Leymann]
+]
+
+[Abstract|
+Aiming to provide the means for efficient collaboration between development and operations personnel, 
+the DevOps paradigm is backed by an increasingly growing collection of tools and reusable artifacts for 
+application management. Continuous delivery pipelines are established based on these building blocks by 
+implementing fully automated, end-to-end application delivery processes, which significantly shorten 
+release cycles to reduce risks and costs as well as gaining a critical competitive advantage. Diverse 
+application environments need to be managed along the pipeline such as development, build, test, and 
+production environments. In this work we address the need for systematically specifying and maintaining 
+diverse application environment topologies enriched with environment-specific requirements in order to 
+implement continuous delivery pipelines. Beside the representation of such requirements, we focus on their 
+systematic and collaborative resolution with respect to the individual needs of the involved application environments.
+]
+
+[Keywords]->[Continuous delivery]
+[Keywords]->[Pipeline ]
+[Keywords]->[Requirements ]
+[Keywords]->[Topology] 
+[Keywords]->[DevOps ]
+
+
+[Title]->[Year]
+[Title]->[Abstract]
+[Abstract]->[Keywords]
+[Title]-[Authors]
