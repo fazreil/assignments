5390f2200f1b0fb9b01d0ac9d48b049233a69522
Fazreil Amreen <fazreil@gmail.com>
2017-12-03 17:37:26 +0800

    more papers

---
+++ lecture3/Benefits_of_Open-Source-Software_in_Defense_Environments.js
@@ -0,0 +1,22 @@
+[Title|
+Benefits of Open Source Software in Defense Environments
+]
+
+[Year|
+2016
+]
+
+[Authors|
+	[Russo, D.]
+]
+
+[Abstract|
+Even though the use of Open Source Software (OSS) might seem paradoxical in Defense environments, this has been proven to be wrong. The use of OSS does not harm security; on the contrary, it enhances it. Even with some drawbacks, OSS is highly reliable and maintained by a huge software community, thus decreasing implementation costs and increasing reliability. Moreover, it allows military software engineers to move away from proprietary applications and single-vendor contracts. Furthermore, it decreases the cost of long-term development and lifecycle management, besides avoiding vendor’s lock in. Nevertheless, deploying OSS deserves an appropriate organization of its life cycle and maintenance, which has a relevant impact on the project’s budget that cannot be overseen. In this paper, we will describe some of the major trends in OSS in Defense environments. The community for OSS has a pivotal role, since it is the core development unit. With Agile and the newest DevOps methodologies, government officials could leverage OSS capabilities, decreasing the Design (or Technical) Debt. Software for Defense purposes could perform better, increase the number of the releases, enhance coordination through the different IT Departments (and the community), and increase release automation, decreasing the probability of errors.
+]
+
+
+
+[Title]->[Year]
+[Title]->[Abstract]
+[Abstract]->[Keywords]
+[Title]-[Authors]
---
+++ lecture3/Quality_Assurance_for_Open-Source-Software_Configuration_Management.js
@@ -0,0 +1,33 @@
+[Title|
+Quality Assurance for Open Source Software Configuration Management
+]
+
+[Year|
+2013
+]
+
+[Authors|
+	[Stefan Meyer]
+	[Philip Healy]
+	[Theo Lynn]
+	[John Morrison]
+]
+
+[Abstract|
+Commonly used open source configuration management systems, such as Puppet, Chef and CFEngine, allow 
+for system configurations to be expressed as scripts. A number of quality issues that may arise when  
+executing these scripts are identified. An automated quality assurance service is proposed that identifies 
+the presence of these issues by automatically executing scripts across a range of environments. Test 
+results are automatically published to a format capable of being consumed by script catalogues and social 
+coding sites. This would serve as an independent signal of script trustworthiness and quality to script 
+consumers and would allow developers to be made quickly aware of quality issues. As a result, potential 
+consumers of scripts can be assured that a script is likely to work when applied to their particular 
+environment. Script developers can be notified of compatibility issues and take steps to address them.
+]
+
+
+
+[Title]->[Year]
+[Title]->[Abstract]
+[Abstract]->[Keywords]
+[Title]-[Authors]
