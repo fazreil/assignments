34f5155f744cdd8464be2f80a1f39d5a0a263452
fazreil <fazreil@gmail.com>
2017-12-03 11:09:36 +0800

    mindmap for houdini

---
+++ lecture3/Continuous_Deployment_for_Complex_Software_Intensive_Industrial_Systems-mindmap.js
@@ -0,0 +1,37 @@
+[Processes|
+	[process]->[develop]
+	[process]->[deliver]
+	[develop]->[software]
+	[deliver]->[software]
+]
+
+[Continuous Deployment|
+	[deploy software]->[automatically]
+	[deploy software]->[continuously]
+]
+
+[stable deployment process] able to cope with -> [domain speciﬁc requirements]
+[stable deployment process] able to cope with -> [domain speciﬁc challenges]
+
+[Main factors] - [high level requirements]
+[Main factors] - [required quality attributes of software product]
+[Main factors] - [challenges in the deployment]
+
+[improvement|
+	[improve]->[quality]
+	[improve]->[productivity]
+]
+
+[Complex Software Intensive Industrial System] ->[large]
+[Complex Software Intensive Industrial System] ->[distributed over heterogeneous platoform]
+[Complex Software Intensive Industrial System] ->[interact with actuator]
+
+[Continuous Deployment]<-[stable deployment process]
+
+
+[improvement] increase->[customer satisfacetion]
+
+[Processes] motivation ->[shorter time to market]
+[Processes] trend ->[Continuous Deployment]
+[Processes] benefit -> [quick deliver]
+[processes] benefit -> [improvement]
